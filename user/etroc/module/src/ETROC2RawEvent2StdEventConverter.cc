0b42f6aa446f39ba9f37e53d6632f34512ec8028
Jordi Duarte-Campderros <jorge.duarte.campderros@cern.ch>
2024-04-24 15:21:33 +0200

    Data converter fully functional. Still debug messages, to be removed them at some point

--- user/etroc/module/src/ETROC2RawEvent2StdEventConverter.cc
+++ user/etroc/module/src/ETROC2RawEvent2StdEventConverter.cc
@@ -9,6 +9,7 @@
 
 static const uint32_t EVENT_HEADER_MASK = 0xFFFFFFF0;
 static const uint32_t EVENT_HEADER = 0xC3A3C3A;
+static const uint32_t EVENT_TRAILER_MASK = 0x3F;
 static const uint32_t EVENT_TRAILER = 0xB;
 
 class ETROCRawEvent2StdEventConverter: public eudaq::StdEventConverter {
@@ -39,7 +40,9 @@ std::vector<uint32_t> GetBack_32bWord(const std::vector<uint8_t>& binary_data) {
 
 bool ETROCRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdEventSP d2, eudaq::ConfigSPC conf) const {
 
-std::cout << "+++++++++++++++++++++++++ " << std::endl;
+    // --- XXX DEBUG --- REMOVE 
+    std::cout << "+++++++++++++++++++++++++ " << std::endl;
+    // --- XXX DEBUG --- REMOVE 
     auto ev = std::dynamic_pointer_cast<const eudaq::RawEvent>(d1);
     size_t nblocks= ev->NumBlocks();
     auto block_n_list = ev->GetBlockNumList();
@@ -48,15 +51,16 @@ std::cout << "+++++++++++++++++++++++++ " << std::endl;
         std::vector<uint8_t> data_block_uint8 = ev->GetBlock(block_n);
         // Convert back into 32bits words 
         std::vector<uint32_t> raw_data = GetBack_32bWord(data_block_uint8);
-std::cout << "Event: " << ev->GetEventNumber() << std::endl;
-for(const uint32_t & kk: raw_data) 
-{
-    std::cout <<  "[0x" << std::setw(8) << std::setfill('0') << std::hex << kk << "]" << std::endl; 
-}
-std::cout << std::dec;
-std::cout << "A -- Number of 32b data: " << raw_data.size() << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        std::cout << "Event: " << ev->GetEventNumber() << std::endl;
+        for(const uint32_t & kk: raw_data) 
+        {
+            std::cout <<  "[0x" << std::setw(8) << std::setfill('0') << std::hex << kk << "]" << std::endl; 
+        }
+        std::cout << std::dec;
+        std::cout << "A -- Number of 32b data: " << raw_data.size() << std::endl;
+        // --- XXX DEBUG --- REMOVE 
 
-//std::cout << "DECODE" << std::endl;
         // Event Header 32bx2 = 64b
         // 1. [28b: 3A3C3A ][4b: Data Mask]
         // Check is the header
@@ -66,7 +70,9 @@ std::cout << "A -- Number of 32b data: " << raw_data.size() << std::endl;
                 << "Event Header" << std::endl;
             return false;
         }
-std::cout << "B" << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        std::cout << "B" << std::endl;
+        // --- XXX DEBUG --- REMOVE 
         const uint16_t data_mask = raw_data[0] & 0xF;
         // The second 32b word;
         // 2 [4b:firmware version][16b:event number[10b:data words][2b:event type]
@@ -75,14 +81,16 @@ std::cout << "B" << std::endl;
         const uint16_t data_words = (raw_data[1] >> 2) & 0x3FF;
         const uint16_t event_type = raw_data[1] & 0x3;
 
-std::cout << "C -- event number:" << event_number << ", event type:" << event_type 
-    << ", version:" << firwmare_version << ", data_mask:" << data_mask << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        std::cout << "C -- event number:" << event_number << ", event type:" << event_type 
+            << ", version:" << firwmare_version << ", data_mask:" << data_mask << std::endl;
+        // --- XXX DEBUG --- REMOVE 
 
         // The last word is the trailer 32b
         // [6b: b001011][12b:hits count][3b:Overflow count][3b:Hamming count][8b:CRC]
         const auto trailer = raw_data[raw_data.size()-1];
         // Malformed ?
-        if( ((trailer >> 26) & 0x3F) != EVENT_TRAILER ) {
+        if( ((trailer >> 26) & EVENT_TRAILER_MASK) != EVENT_TRAILER ) {
             std::cerr << "Potential corrupted data: not found event trailer" << std::endl;
             return false;
         }        
@@ -90,24 +98,18 @@ std::cout << "C -- event number:" << event_number << ", event type:" << event_ty
         const uint16_t overflow_count = (trailer >> 11) & 0x7;
         const uint16_t hamming_count = (trailer >> 8) & 0x7;
         const uint16_t crc = trailer & 0xFF;
-std::cout << "C1 -- hits count:" << hits_count << ", overflow:" << overflow_count
-    << ", hamming:" << hamming_count << ", crc:" << crc << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        std::cout << "C1 -- hits count:" << hits_count << ", overflow:" << overflow_count
+            << ", hamming:" << hamming_count << ", crc:" << crc << std::endl;
+        // --- XXX DEBUG --- REMOVE 
 
 
-        // FIXME --- XXX
-        // And the last word is the trailer
-        //int trailer_event = raw_data[raw_data.size()-1];
-        /// --> XXX - FIXME prov para estos datos sin trailer ?? CHECK TRAILER?a
-        //
-        // NO IDEA WHERE THIS INFO IS???
-        //if( (trailer_event >> 24) & 0xFF != EVENT_TRAILER ) {
-        //    std::cerr << "Potential corrupted data: not found event trailer" << std::endl;
-        //    return false
-        //}
         //const uint16_t etroc_id = trailer_event >> 17
         // XXX -- PROV -- No es aqui!!! Es del frame trailer notdel evnt trailer!
         const uint16_t etroc_id = 0;
-std::cout << "D" << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        std::cout << "D" << std::endl;
+        // --- XXX DEBUG --- REMOVE 
         
         // DATA 40 x data_words + padding bits to create 32bit words
         // 40 x data_words / 32 = number of words
@@ -117,14 +119,18 @@ std::cout << "D" << std::endl;
         etroc_data_words.reserve(data_words);
         // First element with all zeros
         etroc_data_words.push_back(std::bitset<40>());
-std::cout << "E, data words segun el header :" << data_words << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        std::cout << "E, data words segun el header :" << data_words << std::endl;
+        // --- XXX DEBUG --- REMOVE 
         
         // The counter for the 40 word bit
         int counter_40b = 39;
         // The i-esim 40 word 
         int element_40b = 0;
             
-std::cout << "F" << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        std::cout << "F" << std::endl;
+        // --- XXX DEBUG --- REMOVE 
         // Run over the remaining raw data to extract all the data words
         // except the event header (0,1) and the trailer (-1)
         for(int ir = 2; ir < raw_data.size()-2; ++ir) {
@@ -146,23 +152,11 @@ std::cout << "F" << std::endl;
             }
         }
         
-std::cout << "G -- aux_data vector(40b, bitset) size: " << etroc_data_words.size() << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        std::cout << "G -- aux_data vector(40b, bitset) size: " << etroc_data_words.size() 
+            << std::endl;
+        // --- XXX DEBUG --- REMOVE 
         
-/*for(const auto & data_chip: etroc_data_words)
-{
-std::cout << "0x" << std::setw(10) << std::hex << data_chip << std::endl;
-    // Is DATA?
-    if( ((data_chip.to_ulong() >> 39) & 0x1) == 1 )
-{
-std::cout << "EA:" << ((data_chip.to_ulong() >> 37) & 0x3)
-    << ", ROW: " << ((data_chip.to_ulong() >> 29) & 0xf)
-    << ", COL: " << ((data_chip.to_ulong() >> 33) & 0xf)
-    << ", TOA: " << ((data_chip.to_ulong() >> 21) & 0x3ff)
-    << ", CAL: " << (data_chip.to_ulong() & 0x1ff)
-    << std::endl;
-}
-}*/
-//std::cout << "----- END" << std::endl;
         // FIXME ----> GET THE CHIP!! (channel) !!! FIXME -----
         std::vector<uint32_t> eas;
         std::vector<uint32_t> cols;
@@ -170,11 +164,15 @@ std::cout << "EA:" << ((data_chip.to_ulong() >> 37) & 0x3)
         std::vector<uint64_t> toas;
         std::vector<uint32_t> tots;
         std::vector<uint32_t> cals;
-std::cout << "H, Data Words size: " << etroc_data_words.size() 
-    <<  " , " << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        std::cout << "H, Data Words size: " << etroc_data_words.size() 
+            <<  " , " << std::endl;
+        // --- XXX DEBUG --- REMOVE 
         // Looking at the data words ot extract hits
         for(auto & current_word_bitset: etroc_data_words) {
-std::cout << "      [0x" << current_word_bitset << "] " << std::endl;
+            // --- XXX DEBUG --- REMOVE 
+            std::cout << "      [0x" << current_word_bitset << "] " << std::endl;
+            // --- XXX DEBUG --- REMOVE 
             uint64_t current_word = current_word_bitset.to_ulong();
             // Look for data XXX -- FIXME What about the other data? 
             // [1b: 1][2b: EA][4b: COL][4b: ROW][10b: TOA][9b: TOT][10b: CAL]
@@ -193,15 +191,17 @@ std::cout << "      [0x" << current_word_bitset << "] " << std::endl;
         eudaq::StandardPlane plane(etroc_id, "ETROC", "ETROC");
         plane.SetSizeZS(16, 16, 0);
         for(size_t i = 0; i < eas.size(); ++i) {
-std::cout << "   COL: " << cols[i] << " ROW: "<< rows[i] 
-    << " TOT:" << tots[i] << " TOA:" << toas[i] << " CAL:" << cals[i] << std::endl;
+            // --- XXX DEBUG --- REMOVE 
+            std::cout << "   COL: " << cols[i] << " ROW: "<< rows[i] 
+                << " TOT:" << tots[i] << " TOA:" << toas[i] << " CAL:" << cals[i] << std::endl;
+            // --- XXX DEBUG --- REMOVE 
             plane.PushPixel(cols[i],rows[i] , tots[i], toas[i]);
-std::cout << "   ==== > After push pixel: " << eas.size() <<  std::endl;
         }
-std::cout << "   ==== > Plane push_pixel " << std::endl;
         d2->AddPlane(plane);
     }
-std::cout << "\nI" << std::endl;
+    // --- XXX DEBUG --- REMOVE 
+    std::cout << "\nI" << std::endl;
+    // --- XXX DEBUG --- REMOVE 
 
     return true;
 }
