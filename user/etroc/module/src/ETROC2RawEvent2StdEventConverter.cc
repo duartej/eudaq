58cf8450accfbbd0283ae295dd968566ab7c0d1e
teleuser <teleuser@fhltb-rc21.desy.de>
2024-05-08 01:35:05 +0200

    [WIP] Add ability to deal with more than one chip.
    Correlation lost with Mimosa seems to be related with this commit.
    Correlation lost happens only with more (3) boards, not with only
    one.
    Also, provisional printouts are activated.

--- user/etroc/module/src/ETROC2RawEvent2StdEventConverter.cc
+++ user/etroc/module/src/ETROC2RawEvent2StdEventConverter.cc
@@ -1,6 +1,7 @@
 #include "eudaq/StdEventConverter.hh"
 #include "eudaq/RawEvent.hh"
 
+#include<map>
 #include<bitset>
 // --- XXX DEBUG --- REMOVE 
 //#include<iomanip>
@@ -86,8 +87,9 @@ bool ETROCRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdE
         const uint16_t event_type = raw_data[1] & 0x3;
 
         // --- XXX DEBUG --- REMOVE 
-        /*std::cout << "C -- event number:" << event_number << ", event type:" << event_type 
-            << ", version:" << firwmare_version << ", event_mask:" << event_mask << std::endl;*/
+        //*
+	std::cout << "EH -- event number:" << event_number << ", event type:" << event_type 
+            << ", version:" << firwmare_version << ", event_mask:" << event_mask << std::endl;
         // --- XXX DEBUG --- REMOVE 
 
         // The last word is the trailer 32b
@@ -102,19 +104,12 @@ bool ETROCRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdE
         const uint16_t overflow_count = (trailer >> 11) & 0x7;
         const uint16_t hamming_count = (trailer >> 8) & 0x7;
         const uint16_t crc = trailer & 0xFF;
-        // --- XXX DEBUG --- REMOVE 
-        /*std::cout << "C1 -- hits count:" << hits_count << ", overflow:" << overflow_count
-            << ", hamming:" << hamming_count << ", crc:" << crc << std::endl;*/
-        // --- XXX DEBUG --- REMOVE 
-        //
         // XXX -- FIXME -- Missing L1counter and BCID probably needed to monitor
         //                 Also, the ETROC (channel Id), is this info in the frame header? or
         //                 in the frame trailer?
 
 
         //const uint16_t etroc_id = trailer_event >> 17
-        // XXX -- PROV -- No es aqui!!! Es del frame trailer notdel evnt trailer!
-        const uint16_t etroc_id = 0;
         // --- XXX DEBUG --- REMOVE 
         //std::cout << "D" << std::endl;
         // --- XXX DEBUG --- REMOVE 
@@ -131,7 +126,7 @@ bool ETROCRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdE
         // std::cout << "E, data words segun el header :" << data_words << std::endl;
         // --- XXX DEBUG --- REMOVE 
         
-        // The counter for the 40 word bit
+        // The counter for the 40-word bit
         int counter_40b = 39;
         // The i-esim 40 word 
         int element_40b = 0;
@@ -159,20 +154,22 @@ bool ETROCRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdE
                 counter_40b -= 1;
             }
         }
-        
+	// ETROCs attached to a FPGA boards are going to send all the events in order 
+        uint16_t chip_id = 0;
+
         // --- XXX DEBUG --- REMOVE 
         //std::cout << "G -- aux_data vector(40b, bitset) size: " << etroc_data_words.size() 
         //    << std::endl;
         // --- XXX DEBUG --- REMOVE 
         
         // FIXME ----> GET THE CHIP!! (channel) !!! FIXME -----
-        std::vector<uint32_t> eas;
-        std::vector<uint32_t> cols;
-        std::vector<uint32_t> rows;
-        std::vector<uint64_t> toas;
-        std::vector<uint32_t> tots;
-        std::vector<uint32_t> cals;
-        std::vector<uint32_t> l1counter;
+	std::map<uint16_t, std::vector<uint32_t> > eas;
+	std::map<uint16_t, std::vector<uint32_t> > cols;
+        std::map<uint16_t, std::vector<uint32_t> > rows;
+        std::map<uint16_t, std::vector<uint64_t> > toas;
+        std::map<uint16_t, std::vector<uint32_t> > tots;
+        std::map<uint16_t, std::vector<uint32_t> > cals;
+        std::map<uint16_t, std::vector<uint32_t> > l1counter;
         // --- XXX DEBUG --- REMOVE 
         //std::cout << "H, Data Words size: " << etroc_data_words.size() 
         //    <<  " , " << std::endl;
@@ -184,60 +181,97 @@ bool ETROCRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdE
             //std::cout << "      [0x" << current_word_bitset << "] " << std::endl;
             // --- XXX DEBUG --- REMOVE 
             uint64_t current_word = current_word_bitset.to_ulong();
-            // XXX -- What about?
-            // XXX -- FRAME HEADER? 
-            // [1b:0][15b:0x3c5c][2b00][8b:L1Counter][2b:Type][12b:BCID]
+            // XXX -- FRAME HEADER or FRAME FILLER
+            // [1b:0][15b:0x3c5c][2bYY][XXXXXX]
             if( ((current_word >> 24) & CHIP_HEADER_MASK) == CHIP_HEADER )
             {
-                current_l1counter = (((current_word) >> 22 ) & 0xFF) ;
-                /*std::cout << "H1 (CHIP HEADER) " 
-                    << "L1Counter: " << l1counter[l1counter.size()-1]  << " ,"
-                    << "Type" << (((current_word) >> 14 ) & 0x3)  << " ,"
-                    << "BCID: " << ((current_word) & 0xFFF)  
-                    << std::endl;*/
-                continue;
+		// Either is a Frame header [2bYY = 2b00]  or a filler header [2bYY = 2b10]
+		if( (current_word >> 22) & 0x3 == 0x0 ) 
+		{
+                    // FRAME HEADER 
+		    // [1b:0][15b:0x3c5c][2b00][8b:L1Counter][2b:Type][12b:BCID]
+                    current_l1counter = (((current_word) >> 20 ) & 0xFF) ;
+                    //*
+		    std::cout << "   ---| FH (CHIP HEADER) " 
+                       << "L1Counter: " << current_l1counter << ", "  
+                       << "Type: " << (((current_word) >> 12 ) & 0x3)  << ", "
+                       << "BCID: " << ((current_word) & 0xFFF)  
+                       << std::endl;
+                    continue;
+		}
+		else
+		{
+                    // FRAME FILLER
+		    // [1b:0][15b:0x3c5c][2b00][8b:RT_L1Counter][2b:EBS][12b:RT_BCID]
+                    current_l1counter = (((current_word) >> 20 ) & 0xFF) ;
+                    //*
+		    std::cout << "   ---| Frame FILLER " 
+                       << "L1Counter: " << current_l1counter << ", "  
+                       << "Type: " << (((current_word) >> 12 ) & 0x3)  << ", "
+                       << "BCID: " << ((current_word) & 0xFFF)  
+                       << std::endl;
+		    continue;
+		}
             }
             else if( ((current_word >> 39) & 0x1) == 1 )
             {
                 // Look for data XXX -- FIXME What about the other data? See above
                 // [1b: 1][2b: EA][4b: COL][4b: ROW][10b: TOA][9b: TOT][10b: CAL]
-                eas.push_back( (current_word >> 37) & 0x3 );
-                cols.push_back( (current_word >> 33) & 0xf );
-                rows.push_back( (current_word >> 29) & 0xf );
-                toas.push_back( (current_word >> 19) & 0x3ff );
-                tots.push_back( (current_word >> 10) & 0x1ff );
-                cals.push_back( current_word & 0x3ff );
-                l1counter.push_back( current_l1counter );
+                eas[chip_id].push_back( (current_word >> 37) & 0x3 );
+                cols[chip_id].push_back( (current_word >> 33) & 0xf );
+                rows[chip_id].push_back( (current_word >> 29) & 0xf );
+                toas[chip_id].push_back( (current_word >> 19) & 0x3ff );
+                tots[chip_id].push_back( (current_word >> 10) & 0x1ff );
+                cals[chip_id].push_back( current_word & 0x3ff );
+                l1counter[chip_id].push_back( current_l1counter );
+		//*
+		size_t ci = cols.size()-1;
+            	std::cout << "   +++[DATA] COL: " << cols[chip_id][ci] << " ROW: "<< rows[chip_id][ci] 
+			<< " TOT:" << tots[chip_id][ci] << " TOA:" << toas[chip_id][ci] << " CAL:" << cals[chip_id][ci] << std::endl;
+            // --- XXX DEBUG --- REMOVE 
             }
             else 
             {
                 // XXX DEBUG --- REMOVE?
                 // XXX -- And FRAME TRAILER?  Any ohter else
                 // [1b:0][17b:ChipId][6b:Status][8B:hits][8b:CRC]
-                /*std::cout << "H2 (CHIP TRAILER) " 
-                    << "ChipID: " << (((current_word) >> 22 ) & 0x1FFFF)  << " ,"
-                    << "STATuS:" << (((current_word) >> 16 ) & 0x3F)  << " ,"
-                    << "Hits: " << (((current_word) >> 8) & 0xFF) << " ,"
-                    << "CRC: " << ((current_word) & 0xFF) 
-                    << std::endl;*/
+
+                //*
+		std::cout << "   ---| FT (CHIP TRAILER) " 
+                    << "ChipID: " << (((current_word) >> 22 ) & 0x1FFFF)  << ", "
+                    << "STATuS:" << (((current_word) >> 16 ) & 0x3F)  << ", "
+                    << "Hits: " << (((current_word) >> 8) & 0xFF) << ", "
+                    << "CRC: " << ((current_word) & 0xFF) << ", "
+                    << std::endl;
+		chip_id++;
                 // XXX DEBUG --- REMOVE
                 continue;
             }
         }
-        /// FIXME ---MISSING Mechanism for the etroc-id !!~!
-        eudaq::StandardPlane plane(etroc_id, "ETROC", "ETROC");
-        // col, rows, npixels?, frames (l1)
-        plane.SetSizeZS(16, 16, 0, 256);
-        for(size_t i = 0; i < eas.size(); ++i) {
-            // --- XXX DEBUG --- REMOVE 
-            /*std::cout << "   COL: " << cols[i] << " ROW: "<< rows[i] 
-                << " TOT:" << tots[i] << " TOA:" << toas[i] << " CAL:" << cals[i] << std::endl;*/
-            // --- XXX DEBUG --- REMOVE 
-            // XXX --- IT doens't work using the frame... why??
-            // plane.PushPixel(cols[i],rows[i] , tots[i], toas[i], false, l1counter[i]);
-            plane.PushPixel(cols[i],rows[i] , tots[i], toas[i]);
-        }
-        d2->AddPlane(plane);
+	
+	for(const auto & chipid_tots: tots)
+	{
+	    const uint16_t etroc_id = chipid_tots.first;
+            eudaq::StandardPlane plane(etroc_id, "ETROC", "ETROC");
+            // col, rows, npixels?, frames (l1)
+            plane.SetSizeZS(16, 16, 0, 256);
+            for(size_t i = 0; i < eas[etroc_id].size(); ++i) {
+                // --- XXX DEBUG --- REMOVE 
+                // XXX --- IT doens't work using the frame... why??
+	        // l1counter should be there because data is therea
+                // plane.PushPixel(cols[i],rows[i] , tots[i], toas[i], false, l1counter[i]);
+                plane.PushPixel(cols[etroc_id][i],rows[etroc_id][i] , tots[etroc_id][i], toas[etroc_id][i]);
+            }
+            d2->AddPlane(plane);
+	}
+        // --- XXX DEBUG --- REMOVE 
+	//* 
+        std::cout << "ET -- hits count:" << hits_count << ", overflow:" << overflow_count
+            << ", hamming:" << hamming_count << ", crc:" << crc << std::endl;
+        std::cout << "|=====================================================================" 
+		<< "==========================|" << std::endl;
+        // --- XXX DEBUG --- REMOVE 
+        //
     }
     // --- XXX DEBUG --- REMOVE 
     //std::cout << "\nI" << std::endl;
